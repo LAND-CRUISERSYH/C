ec671588287984219fc810a78769eef784349b44
LAND-CRUISERSYH <1354843647@qq.com>
2020-02-24 21:34:34 +0800

    C语言历史与概述练习代码

---
+++ C语言历史与概述/C语言历史与概述/test.c
@@ -0,0 +1,164 @@
+#include<stdio.h>
+
+/*void Test();                //声明Test()函数
+
+void Test()				//函数的定义
+{
+	printf("Hello world!\n");
+}*/
+
+/*
+//编程练习4自定义函数
+void test4_1()
+{
+	printf("今天是晴天!\n");
+}
+
+void test4_2()
+{
+	printf("今天可以出门了!\n");
+}*/
+
+/*
+//编程练习5自定义函数
+void test5_1()
+{
+	printf("西瓜 草莓 ");
+}
+
+void test5_2()
+{
+	printf("葡萄 苹果\n");
+}
+*/
+
+/*
+//编程练习7自定义函数
+void test7()
+{
+	printf("Smile!");
+}
+*/
+
+/*
+//编程练习8自定义函数
+void test8_1();				//函数声明
+void test8_2();
+
+void test8_1()				//函数定义
+{
+	printf("one\n");
+	test8_2();					//函数调用
+	printf("three\n");
+}
+
+void test8_2()
+{
+	printf("two\n");
+}
+*/
+
+int main(void)
+{
+	/*
+	//第一个C语言程序
+	int dogs;
+	printf("How many dogs do you have? ");
+	scanf("%d", &dogs);
+	printf("So you have %d dogs!\n", dogs);
+	*/
+
+	/*
+	//打印Hello world
+	printf("Hello world!\n");
+	*/
+	
+	/*
+	int num = 1;   //定义一个整形变量num并赋值为1
+	printf("I am a simple ");   //使用printf()函数
+	printf("computer.\n");
+	printf("My favorite num is %d becaus it's first.\n", num);
+	*/
+
+	/*
+	//求长方形面积
+	int length, high;          //声明长和宽两个int型变量
+	int area;					  //面积
+	length = 3;				//赋值
+	high = 4;
+	area = length * high;
+	printf("长为: %d,宽为: %d\n", length, high);//打印长和宽
+	printf("面积为: %d", area);		//打印面积
+	*/
+
+	/*
+	Test();                //调用Test()函数
+	*/
+
+	/*
+	//编程练习1.printf()的使用
+	printf("LAND CRUISERSYH\n");
+	printf("LAND\n");
+	printf("CRUISERSYH\n");
+	printf("LAND ");
+	printf("CRUISERSYH");
+	*/
+
+	/*
+	//编程练习2.打印姓名和地址
+	printf("My name is LAND CRUISERSYH\n");
+	printf("address: SHANXI\n");
+	*/
+
+	/*
+	编程练习3.年龄转为天数
+	int age = 22;       //年龄
+	int day = age * 365;				//天数
+	printf("我的年龄为: %d,合计天数为: %d\n", age, day);
+	*/
+
+	/*
+	//编程练习4.自定义两个函数并在main()函数里调用
+	test4_1();			//调用test4_1()函数
+	test4_1();
+	test4_1();
+	test4_2();			//调用test4_2()函数
+	*/
+
+	/*
+	//编程练习5.自定义两个函数并在main()函数里调用
+	test5_1();
+	test5_2();
+	test5_2();
+	test5_1();
+	*/
+
+	/*
+	//编程练习6.变量值运算及打印
+	int num = 10;
+	printf("num = %d\n", num);
+	printf("2*num = %d\n", 2 * num);
+	printf("num*num = %d\n", num*num);
+	*/
+	
+	/*
+	//编程练习7.转义字符的使用
+	test7();
+	test7();
+	test7();
+	printf("\n");
+	test7();
+	test7();
+	printf("\n");
+	test7();
+	*/
+
+	/*
+	//编程练习8.自定义函数相互调用问题
+	printf("Starting now: \n");
+	test8_1();
+	printf("done!\n");
+	*/
+
+	return 0;
+}
