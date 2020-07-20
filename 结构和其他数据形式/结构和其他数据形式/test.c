364f4c2b99c893b81a4a71c240c81f2bb99c077a
LAND-CRUISERSYH <1354843647@qq.com>
2020-07-21 03:12:58 +0800

    结构和其他数据形式

--- 结构和其他数据形式/结构和其他数据形式/test.c
+++ 结构和其他数据形式/结构和其他数据形式/test.c
@@ -3,6 +3,7 @@
 
 #define BOOK_NAME 41
 #define AUTHOR 31
+#define SIZE 20
 
 char *s_gets(char *s, int n);
 
@@ -12,8 +13,38 @@ struct book
 	char author[31];
 	float price;
 };
+
+struct names
+{
+	char first[SIZE];
+	char last[SIZE];
+};
+
+struct guy
+{
+	struct names handle;
+	char favfood[SIZE];
+	char job[SIZE];
+	float income;
+};
 int main()
 {
+	printf("%d", sizeof(float));
+	/*
+	struct guy fellow[2] = 
+	{
+		{{"hello","world"},"salmon","personality coach",68712.00},
+		{{"Rodney","Swillbelly"},"tripe","tabloid editor",432400.00}
+	};
+	struct guy *him;              //结构体指针
+	printf("&fellow[0] = %p,&fellow[1] = %p\n", &fellow[0], &fellow[1]);
+	him = &fellow[0];
+	printf("him = %p,him+1 = %p\n", him,him+1);
+	printf("him->income = %.2f,(*him).income = %.2f\n", him->income, (*him).income);
+	him++;
+	printf("him->favfood = %s,him->handle->first = %s\n",him->favfood,him->handle.first);
+	*/
+	/*
 	struct book library;
 	printf("请输入书名: ");
 	s_gets(library.title, BOOK_NAME);
@@ -23,6 +54,7 @@ int main()
 	scanf("%f", &library.price);
 	printf("书名为: %s,作者为: %s,价格为: %.2f", library.title, library.author, library.price);
 	//printf("hello world\n");
+	*/
 	return 0;
 }
 
