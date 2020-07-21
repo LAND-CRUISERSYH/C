ad0968075b8344d2cafc961d46bf2bcd40ba2465
LAND-CRUISERSYH <1354843647@qq.com>
2020-07-21 22:57:14 +0800

    结构和其他数据形式

--- 结构和其他数据形式/结构和其他数据形式/test.c
+++ 结构和其他数据形式/结构和其他数据形式/test.c
@@ -1,35 +1,85 @@
 #include<stdio.h>
 #include<string.h>
+#include<stdlib.h>
 
-#define BOOK_NAME 41
-#define AUTHOR 31
+//#define BOOK_NAME 41
+//#define AUTHOR 31
+//#define SIZE 20
 #define SIZE 20
 
 char *s_gets(char *s, int n);
+void get_str(struct str* p);
+void make_str(struct str* p);
+void put_str(const struct str* p);
+void free_str(struct str* p);
+void show_flex(const struct flex* p);
 
-struct book
+struct str
 {
-	char title[41];
-	char author[31];
-	float price;
+	char *firstname;
+	char *lastname;
+	int letters;
 };
 
-struct names
+struct flex
 {
-	char first[SIZE];
-	char last[SIZE];
+	int count;
+	double average;
+	double scores[];
 };
-
-struct guy
-{
-	struct names handle;
-	char favfood[SIZE];
-	char job[SIZE];
-	float income;
-};
+//struct book
+//{
+//	char title[41];
+//	char author[31];
+//	float price;
+//};
+//
+//struct names
+//{
+//	char first[SIZE];
+//	char last[SIZE];
+//};
+//
+//struct guy
+//{
+//	struct names handle;
+//	char favfood[SIZE];
+//	char job[SIZE];
+//	float income;
+//};
 int main()
 {
-	printf("%d", sizeof(float));
+	int n = 5;
+	double sum = 0;
+	struct flex *p1, *p2;
+	p1 = malloc(sizeof(struct flex)+ n * sizeof(double));
+	p1->count = n;
+	for (int i = 0; i < p1->count; i++)
+	{
+		p1->scores[i] = 20.0 - i;
+		sum += p1->scores[i];
+	}
+	p1->average = sum / n;
+	show_flex(p1);
+	n = 9;
+	sum = 0;
+	p2 = malloc(sizeof(struct flex) + n * sizeof(double));
+	p2->count = n;
+	for (int i = 0; i < p2->count; i++)
+	{
+		p2->scores[i] = 20.0 - i / 2.0;
+		sum += p2->scores[i];
+	}
+	p2->average = sum / n;
+	show_flex(p2);
+	/*
+	struct str person;
+	get_str(&person);
+	make_str(&person);
+	put_str(&person);
+	free_str(&person);
+	*/
+	//printf("%d", sizeof(float));
 	/*
 	struct guy fellow[2] = 
 	{
@@ -58,6 +108,14 @@ int main()
 	return 0;
 }
 
+void show_flex(const struct flex* p)
+{
+	printf("Scores: ");
+	for (int i = 0; i < p->count; i++)
+		printf("%.2f ", p->scores[i]);
+	printf("\nAverage: %.2f\n", p->average);
+}
+
 char *s_gets(char *s, int n)
 {
 	char *p;
@@ -75,4 +133,30 @@ char *s_gets(char *s, int n)
 		}
 	}
 	return p;
+}
+
+void get_str(struct str* p)
+{
+	char temp[SIZE];
+	printf("请输入你的姓: ");
+	s_gets(temp,SIZE);
+	p->firstname = (char *)malloc(strlen(temp) + 1);
+	strcpy(p->firstname, temp);
+	printf("请输入你的名: ");
+	s_gets(temp, SIZE);
+	p->lastname = (char *)malloc(strlen(temp) + 1);
+	strcpy(p->lastname, temp);
+}
+void make_str(struct str* p)
+{
+	p->letters = strlen(p->firstname) + strlen(p->lastname);
+}
+void put_str(const struct str* p)
+{
+	printf("%s %s: %d\n", p->firstname, p->lastname, p->letters);
+}
+void free_str(struct str* p)
+{
+	free(p->firstname);
+	free(p->lastname);
 }
